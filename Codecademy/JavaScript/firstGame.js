86fdcedf6c011f3d62a48f1b1ba4a9b60c578d67
Kyle Hunter <kyle.hunter91@gmail.com>
2016-02-13 12:38:09 -0500

    Adding my firstGame.js

---
+++ Codecademy/JavaScript/firstGame.js
@@ -0,0 +1,14 @@
+// Check if the user is ready to play!
+
+confirm('Are you ready to play?');
+
+age = prompt("What's your age?");
+
+if(age<13)
+{
+    console.log("You're allowed to play this game, but the developer of this game takes no responsibility for what you are about to see.");
+}
+else
+{
+    console.log("Let's get started!");
+}
