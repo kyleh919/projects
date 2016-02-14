88a1f35ffe2d15b4496bc64ea11cdbd9aae47c4b
Kyle Hunter <kyle.hunter91@gmail.com>
2016-02-14 16:54:25 -0500

    Updated rps with choice of rope and prompt for new choice if a tie exists

--- Codecademy/JavaScript/rps.js
+++ Codecademy/JavaScript/rps.js
@@ -1,33 +1,42 @@
-var userChoice = prompt("Do you choose rock, paper or scissors?");
+var userChoice = prompt("Do you choose rock, paper, rope or scissors?");
+
 var computerChoice = Math.random();
-if (computerChoice < 0.34)
+
+if (computerChoice < 0.25)
 {
 	computerChoice = "rock";
 }
-else if(computerChoice <= 0.67)
+else if(computerChoice >= 0.25 && computerChoice < 0.5)
 {
 	computerChoice = "paper";
 }
-else
+else if(computerChoice >= 0.5 && computerChoice < 0.75)
 {
 	computerChoice = "scissors";
 }
+else
+{
+    computerChoice = "rope";
+}
 
 console.log("Computer: " + computerChoice);
-
-compare(userChoice, computerChoice);
 
 var compare = function(choice1, choice2)
 {
     if(choice1 === choice2)
     {
-        return "The result is a tie!";
+        //return "The result is a tie!";
+        choice1 = prompt("The result is a tie! Please resubmit a new entry: do you choose rock, paper, rope or scissors?");
     }
-    else if(choice1 === "rock")
+    if(choice1 === "rock")
     {
         if(choice2 === "scissors")
         {
             return "rock wins";
+        }
+        else if(choice2 === "rope")
+        {
+            return "rope wins";
         }
         else
         {
@@ -40,6 +49,10 @@ var compare = function(choice1, choice2)
         {
             return "paper wins";
         }
+        else if(choice2 === "rope")
+        {
+            return "rope wins";
+        }
         else
         {
             return "scissors wins";
@@ -51,13 +64,24 @@ var compare = function(choice1, choice2)
         {
             return "rock wins";
         }
+        else if(choice2 === "rope")
+        {
+            return "rope wins";
+        }
         else
         {
             return "scissors wins";
         }
+    }
+    else if(choice1 ==="rope")
+    {
+        return "rope wins";
     }
     else
     {
         return "I'm afraid I don't know what that is!";
     }
 };
+
+
+compare(userChoice, computerChoice);
