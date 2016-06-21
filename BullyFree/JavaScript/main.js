5372fe69d4b184fe81b6d918391ba56dbda867b0
Kyle Hunter <kyle.hunter91@gmail.com>
2016-06-20 22:38:33 -0400

    Minor adjustments for starting a new game.

--- BullyFree/JavaScript/main.js
+++ BullyFree/JavaScript/main.js
@@ -25,10 +25,11 @@ Lose - get picked on by Tommy Twotone
 */
 
 
+var userReady = "undefined";
 
 
 
-
+/******TEST CODE******/
 var zack = new classmate("Zack");
 
 console.log(zack.name);
@@ -36,6 +37,35 @@ console.log(zack.name);
 zack.action = zack.randomAction();
 //console.log(zack.randomAction());
 console.log(zack.action);
+
+newGame();
+/*********************/
+
+
+function newGame() {
+
+	do{
+		userReady = prompt("Are you willing to do what it takes to take down Tommy Twotone?");
+
+		if(userReady === "Yes" || userReady === "yes") {
+			console.log("yes");
+			//startGame();
+		}
+		else if(userReady === "No" || userReady === "no") {
+			console.log("no");
+			confirm("Thanks for not helping, Tommy Twotone will own Brown High School forever!");
+		}
+		else {
+			console.log("undefined");
+
+			alert("Sorry, your input is undefined. Please try again and respond with either Yes or No.");
+			newGame();
+		};
+	}
+	while(userReady == "undefined");
+
+};
+
 
 
 
