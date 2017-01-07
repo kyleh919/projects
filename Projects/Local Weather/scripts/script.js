fc5b5c8342a841b53109f48082a494a3fdfa3c82
Kyle Hunter <kyle.hunter91@gmail.com>
2017-01-07 12:39:18 -0500

    Updating local weather page

--- Projects/Local Weather/scripts/script.js
+++ Projects/Local Weather/scripts/script.js
@@ -10,25 +10,50 @@ $(document).ready(function(){
         navigator.geolocation.getCurrentPosition(function(position) {
             myLat = position.coords.latitude;
             myLong = position.coords.longitude;
-        
-            $(".test1").html("latitude: " + myLat + "<br>longitude: " + myLong);
-            
+                    
             wurl = "http://api.openweathermap.org/data/2.5/weather?lat="+myLat+"&lon="+myLong+"&appid=b23c7f46089700848d48456167e7329c"
 
             console.log(wurl);
 
             $.getJSON(wurl, function(json){
                 var tempK;
+                var tempF;
+                var tempC;
+                var loc;
 
                 console.log(json);
 
+                loc = json.name;
+                cond = json.weather[0].main;
+
                 tempK = json.main.temp;
-                console.log(tempK);
+                console.log("kelvin = " + tempK);
 
+                tempF = Math.round(tempK * 9/5 - 459.67);
+                console.log("fahrenheit = " + tempF);
+
+                tempC = Math.round(tempK - 273.15);
+                console.log("celsius = " + tempC);
+
+                // default values to show when page loads
+                $("#loc").html(loc);
+                $("#cond").html(cond);
+                $("#temp").html(tempF + "°F");
+
+                $("#fBtn").click(function(){
+                    $("#temp").html(tempF + "°F");
+                });
+                $("#cBtn").click(function(){
+                    $("#temp").html(tempC + "°C");
+                });
+                
             });
-
+            
         });
+    
     }
+
+    
 
 
 });
