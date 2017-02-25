589fe38209d50bd287982c3d18813f47a88a470c
Kyle Hunter <kyle.hunter91@gmail.com>
2017-02-25 10:48:28 -0500

    Updating algorithms

--- Learning/Free Code Camp/Front-End/JavaScript/intermediateAlgoScripting.js
+++ Learning/Free Code Camp/Front-End/JavaScript/intermediateAlgoScripting.js
@@ -1,3 +1,10 @@
+/*
+We'll pass you an array of two numbers. Return the sum of those two numbers
+and all numbers between them.
+
+The lowest number will not always come first
+*/
+
 function sumAll(arr) {
  
   var min = Math.min(arr[0], arr[1]);
@@ -12,3 +19,41 @@ function sumAll(arr) {
 }
  
 sumAll([1, 4]);
+
+
+
+/*
+Compare two arrays and return a new array with any items only found in one of the two
+given arrays, but not both. In other words, return the symmetric difference of the two arrays.
+*/
+
+function diffArray(arr1, arr2) {
+  
+  var newArr = [];
+  var arr1Len = arr1.length;
+  var arr2Len = arr2.length;
+  var temp;
+  var index = 0;
+  
+  
+  for(var i=0; i<arr1Len; i++) {
+    temp = arr2.indexOf(arr1[i]);
+    if(temp === -1) {
+      newArr[index] = arr1[i];
+      index++;
+    }
+  }
+  
+  
+  for(i=0; i<arr2Len; i++) {
+    temp = arr1.indexOf(arr2[i]);
+    if(temp === -1) {
+      newArr[index] = arr2[i];
+      index++;
+    }
+  }
+  
+  return newArr;
+}
+
+diffArray([1, 2, 3, 5], [1, 2, 3, 4, 5]);
