e13ead503b2a08604b386710c51fa65eb1f1e3c2
Kyle Hunter <kyle.hunter91@gmail.com>
2017-07-12 21:10:18 -0700

    Updating twitch tv app

--- Projects/TwitchTV/script.js
+++ Projects/TwitchTV/script.js
@@ -1,26 +1,55 @@
 
 var turl = "https://wind-bow.gomix.me/twitch-api"
-var defaultUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+var defaultUsers = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
 
 
-function getUrl(input) {
-    return turl + "/users/" + input;
+
+function getData(type, id) {
+    
+    var url = "https://wind-bow.glitch.me/twitch-api/users/freecodecamps";//freecodecamp";
+    console.log("user = " + id);
+
+    $.getJSON(url, function(result) {
+        console.log(result);
+
+        var logo = result.logo;
+        var display_name = result.display_name;
+        var name = result.name;
+
+        console.log("display_name = " + display_name);
+
+
+    });
 }
 
-// function defaultStatus() {
 
-// }
+function defaultDisplay() {
+    
+    var colContainer, contentContainer, containerFluidSty;
+    var numDefaults = defaultUsers.length;
+
+    // add style to show border, that its been added
+    containerFluidSty = "background-color: blue;";
+
+    colContainer = document.createElement("div");
+    colContainer.setAttribute("class", "container-fluid");
+    colContainer.setAttribute("id", containerFluidSty);
+    contentContainer = document.getElementById("content");
+    contentContainer.appendChild(colContainer);
+
+    for(var user in defaultUsers) {
+        var currUser = defaultUsers[user];
+
+        getData("users", currUser);
+        
+
+
+    }
+}
 
 
 $(document).ready(function(){
-
-
-    console.log("turl = " + turl);
-    console.log("default user list = " + defaultUsers);
-
-    // defaultStatus();
-    var newUrl = getUrl("freecodecamp");
-    console.log(newUrl);
+    defaultDisplay();
 
 
 });
